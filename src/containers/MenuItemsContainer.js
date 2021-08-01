06afb29424c3c1553ceef7edb549d3c45c0b7f24
Vicki Chen <missvickies14@gmail.com>
2021-07-31 23:43:06 -0400

    connected props to state

---
+++ src/containers/MenuItemsContainer.js
@@ -0,0 +1,9 @@
+import { MenuItems } from '../components/MenuItems';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  };
+};
+export const MenuItemsContainer = connect(mapStateToProps)(MenuItems);
--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -2,7 +2,7 @@ let id = 1;
 
 export const initialItems = [
   { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
+  { uuid: id++, name: 'Vegan Ham sandwich', price: 12, quantity: 1 }
 ];
 
 export const reducer = (state = initialItems, action) => {
